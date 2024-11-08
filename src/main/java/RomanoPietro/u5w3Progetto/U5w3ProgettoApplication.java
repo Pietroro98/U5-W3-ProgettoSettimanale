ddcfa90f3be8f2f1e2afb1f7448dafb4e090ae18
Pietroro98 <pietroro98@gmail.com>
2024-11-08 12:02:30 +0100

    Commit Iniziale

---
+++ src/main/java/RomanoPietro/u5w3Progetto/U5w3ProgettoApplication.java
@@ -0,0 +1,13 @@
+package RomanoPietro.u5w3Progetto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w3ProgettoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w3ProgettoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/RomanoPietro/u5w3Progetto/U5w3ProgettoApplicationTests.java
@@ -0,0 +1,13 @@
+package RomanoPietro.u5w3Progetto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w3ProgettoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
